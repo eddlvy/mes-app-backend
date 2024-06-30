811d40e31a274415f7ea0ec5022510dab36a0e63
Eduardo Levy <elevypo@gmail.com>
2024-06-30 18:36:14 +0300

    Url Fixed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT;
 app.use(express.json())
 app.use(cors())
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://mes-app.netlify.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS");
 
